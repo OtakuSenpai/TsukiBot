e5c5f4dc2c48e5ee96abd400817ef187af0ace73
OtakuSenpai <Find me at Freenode>
2017-10-02 14:10:22 +0530

    Ping Plugin finally works perfectly.Changed HISTORY,TODO to reflect correct timelines.Added another note in README.md

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <chrono>
+#include <cstdlib>
 
 #include "TsukiIRC.hpp"
 
@@ -7,19 +7,25 @@ using namespace Tsuki;
 
 int main()
 {
-  std::string server{"irc.freenode.net"},nick{"G33kb0i"},realname{"G33kb0i"},pass=" ";
-  std::string user{"Bhsdns"},channel{"#cplusplus.com"};
-  unsigned int port = 6667;
+  try {
+    std::string server{"irc.freenode.net"},nick{"G33kb0i"},realname{"G33kb0i"},pass=" ";
+    std::string user{"Bhsdns"},channel{"##lazy-valoran"};
+    unsigned int port = 6667;
 
-  Channel chan(channel);
-  User usr(user);
-  std::string msg;
+    Channel chan(channel);
+    User usr(user);
+    std::string msg;
 
-  Bot *bot = new Bot(server,channel,nick,user,pass,port);
-  bot->Connect();
+    Bot *bot = new Bot(server,channel,nick,user,pass,port);
+    bot->Connect();
 
-  bot->Disconnect();
-  std::cout<<"Exiting program now."<<std::endl;
-  delete bot;
-  return 0;
+    bot->Disconnect();
+    std::cout<<"Exiting program now."<<std::endl;
+    delete bot;
+    return 0;
+  }
+  catch(const std::exception& e) {
+	std::cout<<"Caught exception: \n"<<e.what();
+	std::exit(EXIT_FAILURE);
+  }
 }
--- plugins-src/PingPlugin/pingplg.cpp
+++ plugins-src/PingPlugin/pingplg.cpp
@@ -15,8 +15,8 @@ PingPlg :: ~PingPlg() {
 
 char* PingPlg :: onCommand(const char* name) {
   const char* tempVar = "\001ACTION pings ";
-  char* retValue = (char*) calloc(1,strlen(name) + strlen(tempVar) + 5);
+  char* retValue = (char*) calloc(1,strlen(name) + strlen(tempVar) + 9);
   strcpy(retValue,tempVar); strcat(retValue,name);
-  strcat(retValue,"\r\n");
+  strcat(retValue,"\001\r\n");
   return retValue;
 }		
