91e89771e5b6a010f9b878e0555c631bd86d2202
OtakuSenpai <edu.avraneel@gmail.com>
2017-10-15 08:02:20 +0530

    Made the bot reconnect after a ping timeout(maybe),solving the issue of ping timouts.Have to fix the ping plugin though.

--- main.cpp
+++ main.cpp
@@ -7,25 +7,19 @@ using namespace Tsuki;
 
 int main()
 {
-  try {
-    std::string server{"irc.freenode.net"},nick{"G33kb0i"},realname{"G33kb0i"},pass=" ";
-    std::string user{"Bhsdns"},channel{"##lazy-valoran"};
-    unsigned int port = 6667;
+  std::string server{"irc.freenode.net"},nick{"G33kb0i"},realname{"G33kb0i"},pass;
+  std::string user{"OtakuSenpai"},channel{"##llamas"},nick2{"G33k1"};
+  unsigned int port = 6667;
 
-    Channel chan(channel);
-    User usr(user);
-    std::string msg;
+  Channel chan(channel);
+  User usr(user);
+  std::string msg;
 
-    Bot *bot = new Bot(server,channel,nick,user,pass,port);
-    bot->Connect();
+  Bot *bot = new Bot(server,channel,nick,nick2,user,realname,pass,port);
+  bot->Connect();
 
-    bot->Disconnect();
-    std::cout<<"Exiting program now."<<std::endl;
-    delete bot;
-    return 0;
-  }
-  catch(const std::exception& e) {
-	std::cout<<"Caught exception: \n"<<e.what();
-	std::exit(EXIT_FAILURE);
-  }
+  bot->Disconnect();
+  std::cout<<"Exiting program now."<<std::endl;
+  delete bot;
+  return 0;
 }
